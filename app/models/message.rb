3651e19c2a45797dfb53bf7097de29aff620ba01
saatoko <rm-as.l2j@ezweb.ne.jp>
2019-11-07 17:43:26 +0900

    サイドバーに最新のメッセージを表示する、ヘッダーにグループ名とユーザー名が表示されるようにする

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,16 @@ class Message < ApplicationRecord
 
   validates :content, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
+
+  # def show_current_title
+  #   @messages.each do |title|
+  #     title.group_id 
+  #   end
+  # end
+
+  # def show_current_member
+  #   @groups.each do |member|
+  #     member.user_id
+  #   end
+  # end
 end
