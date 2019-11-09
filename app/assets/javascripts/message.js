984f01f9b7a2f3f300911844ab3e0abab9026b64
saatoko <rm-as.l2j@ezweb.ne.jp>
2019-11-09 20:19:50 +0900

    3. 2のイベントが発火したときにAjaxを使用して、messages#createが動くようにする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,15 @@
 $(function(){
-  $(".new_message").on("submit", function(){
+  $(".new_message").on("submit", function(e){
     e.preventDefault()
-    // console.logを用いてイベント発火しているか確認
-    // 例　console.log("maru");
+    var formData = new FormData(this);
+    var url = $(this).attr('action')  
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 })
