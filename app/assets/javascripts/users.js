302c51e0fca3e083197fb6a5cf2e3584a0528e1d
saatoko <rm-as.l2j@ezweb.ne.jp>
2019-11-10 22:13:43 +0900

    非同期通信の結果を得て、HTMLを作成する, 作成したHTMLをビュー上に描画する,　エラー時の処理を行う

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,33 @@
 $(function() {
+  function addUser(user) {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+    // console.log(html)
+  }
+
+  function addNoUser() {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+    // console.log(html)
+  }
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="ChatMember clearfix" id="${id}">
+      <p class="ChatMember__name">${name}</p>
+      <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".ChatMembers").append(html);
+    // console.log(html)
+  }
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     $.ajax({
@@ -8,11 +37,23 @@ $(function() {
       dataType: "json"
     })
     .done(function(users) {
-      console.log(users);
-      console.log("成功です");
-    })
+      $("#user-search-result").empty();
+
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          addUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }
+      // console.log(users);     
+    }) 
     .fail(function() {
-      console.log("失敗です");
-    });
+      alert("ユーザー検索に失敗しました");
+    })  
   });
 });
+
+
