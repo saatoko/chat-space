183defdb336a807de8e77689ef67feec1e64a809
saatoko <rm-as.l2j@ezweb.ne.jp>
2019-11-10 16:22:43 +0900

    delete comment out

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -36,9 +36,7 @@ $(function(){
     })
       .done(function(data){
         var html = buildHTML(data);
-        // console.log(data)
         $('.messages').append(html);  
-        // console.log(html)    
         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');  
         $('form')[0].reset();
         $('#button').prop('disabled', false);
@@ -49,60 +47,5 @@ $(function(){
       return false;
   });
 });
-
-
-// var a = 1;
-    // if (a == 1) {
-    //   "true"
-    // } else {
-    //   "false"
-    // }
-    // a == 1 ? "true": "false"
-
-    // if ( message.image ) {
-    //   var image = <img src=${message.image} ></img>
-    //  } else {
-    //    var image = ""
-    //  }
-
-    // if ( message.image ) {
-    //   var html =
-    //    `<div class="message" data-message-id=${message.id}>
-    //       <div class="upper-message">
-    // //         <div class="upper-message__user-name">
-    // //           ${message.user_name}
-    // //         </div>
-    // //         <div class="upper-message__date">
-    // //           ${message.date}
-    // //         </div>
-    // //       </div>
-    // //       <div class="lower-message">
-    // //         <p class="lower-message__content">
-    // //           ${message.content}
-    // //         </p>
-    // //       </div>
-    // //       <img src=${message.image} >
-    // //     </div>`
-    //   return html;
-    // } else {
-    //   var html =
-    //    `<div class="message" data-message-id=${message.id}>
-    //       <div class="upper-message">
-    //         <div class="upper-message__user-name">
-    //           ${message.user_name}
-    //         </div>
-    //         <div class="upper-message__date">
-    //           ${message.date}
-    //         </div>
-    //       </div>
-    //       <div class="lower-message">
-    //         <p class="lower-message__content">
-    //           ${message.content}
-    //         </p>
-    //       </div>
-    //     </div>`
-    //   return html;
-    // };
-
 
 
